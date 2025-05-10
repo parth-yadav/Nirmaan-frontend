c383d324fa362112ee73fbb87607ef10d130e901
Parth <parthmmyadav@gmail.com>
2025-05-10 11:23:30 +0530

    sections in test font fixed

--- src/components/Test/TestComponent/TestSections.jsx
+++ src/components/Test/TestComponent/TestSections.jsx
@@ -5,17 +5,29 @@ function TestSections({ data }) {
         <table className="min-w-full table-fixed">
           <thead>
             <tr>
-              <th className="w-2/5  text-gray-500 text-left">Sections</th>
-              <th className="w-1/5  text-gray-500 text-left">Question</th>
-              <th className="w-2/5  text-gray-500 text-left">Maximum Marks</th> 
+              <th className="w-2/5 text-sm  text-gray-500 text-left">
+                Sections
+              </th>
+              <th className="w-1/5  text-sm  text-gray-500 text-left">
+                Question
+              </th>
+              <th className="w-2/5 text-sm  text-gray-500 text-left">
+                Maximum Marks
+              </th>
             </tr>
           </thead>
           <tbody>
             {data.sections.map((section, index) => (
               <tr key={index}>
-                <td className="w-1/3 py-1 truncate">{section.title}</td>
-                <td className="w-1/3 py-1 truncate">{section.questions}</td>
-                <td className="w-1/3 py-1 truncate">{section.maximum_marks}</td>
+                <td className="w-1/3 text-base py-1 truncate">
+                  {section.title}
+                </td>
+                <td className="w-1/3 text-base py-1 truncate">
+                  {section.questions}
+                </td>
+                <td className="w-1/3 text-base py-1 truncate">
+                  {section.maximum_marks}
+                </td>
               </tr>
             ))}
           </tbody>
